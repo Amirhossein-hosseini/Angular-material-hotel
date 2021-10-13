0d7040dfe38de1f5e6f50daf73e076579db19b02
Amirhossein hosseini <amyrhsynhsyny703@gmail.com>
2021-10-13 16:01:01 +0330

    header complete...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+  show:boolean = false;
+  Toggle(){
+    this.show = ! this.show
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,12 +5,19 @@ import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatInputModule} from '@angular/material/input'
 import {MatToolbarModule} from '@angular/material/toolbar'
 import{MatIconModule} from '@angular/material/icon'
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatListModule} from '@angular/material/list'
+import {MatButtonModule} from '@angular/material/button'
 const Model = [
 MatCardModule,
 MatToolbarModule,
 MatFormFieldModule,
 MatInputModule,
-MatIconModule
+MatIconModule,
+MatSidenavModule,
+MatListModule,
+MatButtonModule
+
 ]
 
 @NgModule({
